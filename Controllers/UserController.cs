0c04fb692f9444977e950b3aebfd18f5e5cdeac5
Flávio <f.augustosdn@gmail.com>
2024-03-27 17:30:20 -0300

    adicionada primeira função e ajustado endpoint para ser utilizado com letra minúscula

---
+++ Controllers/UserController.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace MyFirstAPI.Controllers;
+[Route("api/[controller]")]
+[ApiController]
+public class UserController : ControllerBase
+{
+    [HttpGet]
+    public IActionResult GetUser()
+    {
+        return Ok("Primeira API");
+    }
+}
